573b646e90fb6b47a407c66542f985a1f2a6f42f
Saira-Akhter <saira.akhter@icloud.com>
2023-12-15 02:34:46 +0000

    gallery checkbox finished

--- src/content-handlers/iiif/modules/uv-contentleftpanel-module/GalleryView.ts
+++ src/content-handlers/iiif/modules/uv-contentleftpanel-module/GalleryView.ts
@@ -18,100 +18,117 @@ export class GalleryView extends BaseView {
 
     this.$gallery = $('<div class="iiif-gallery-component"></div>');
 
-   
-    this.$extendLabelsCheckbox = $('<input type="checkbox" id="extendLabelsCheckbox">');
-    const $label = $('<label for="extendLabelsCheckbox">Show full labels</label>');
-
-    
-    this.$element.append(this.$extendLabelsCheckbox);
-    this.$element.append($label);
     this.$element.append(this.$gallery);
+
     
-    
-    this.$extendLabelsCheckbox.on('change', () => {
-      const extendLabels = this.$extendLabelsCheckbox.prop('checked');
-      this.toggleLabelExtension(extendLabels);
-    });
-  }
-
-  toggleLabelExtension(extendLabels: boolean): void {
-    if (extendLabels) {
-      
-      this.$gallery.find('.info .label').css({
-        'overflow-x': 'visible',
-        'text-overflow': 'nowrap',
-        'white-space': 'break-spaces',
-        'max-width': '100%',
-      });
-    } else {
-      
-      this.$gallery.find('.info .label').css({
-        'overflow-x': 'hidden',
-        'text-overflow': 'ellipsis',
-        'white-space': 'nowrap',
-        'max-width': '100%',
-      });
-    }
-  }
-
-  public setup(): void {
-    const that = this;
-
-    this.galleryComponent = new GalleryComponent({
-      target: <HTMLElement>this.$gallery[0],
-    });
-
-    this.galleryComponent.on(
-      "thumbSelected",
-      function(thumb: any) {
-        that.extensionHost.publish(IIIFEvents.GALLERY_THUMB_SELECTED, thumb);
-        that.extensionHost.publish(IIIFEvents.THUMB_SELECTED, thumb);
-      },
-      false
-    );
-
-    this.galleryComponent.on(
-      "decreaseSize",
-      function() {
-        that.extensionHost.publish(IIIFEvents.GALLERY_DECREASE_SIZE);
-      },
-      false
-    );
-
-    this.galleryComponent.on(
-      "increaseSize",
-      function() {
-        that.extensionHost.publish(IIIFEvents.GALLERY_INCREASE_SIZE);
-      },
-      false
-    );
-  }
-
-  public databind(): void {
-    this.galleryComponent.options.data = this.galleryData;
-    this.galleryComponent.set(this.galleryData);
-    this.resize();
-  }
-
-  show(): void {
-    this.isOpen = true;
-    this.$element.show();
-
-    // todo: would be better to have no imperative methods on components and use a reactive pattern
     setTimeout(() => {
-      this.galleryComponent.selectIndex(this.extension.helper.canvasIndex);
-    }, 10);
+      const labelHasOverflow = this.checkLabelOverflow();
+
+      if (labelHasOverflow) {
+        this.$extendLabelsCheckbox = $('<input type="checkbox" id="extendLabelsCheckbox">');
+        const $label = $('<label for="extendLabelsCheckbox">Show full labels</label>');
+
+        this.$element.prepend($label);
+        this.$element.prepend(this.$extendLabelsCheckbox);
+
+        this.$extendLabelsCheckbox.on('change', () => {
+          const extendLabels = this.$extendLabelsCheckbox.prop('checked');
+          this.toggleLabelExtension(extendLabels);
+        });
+      }
+    }, 0);
   }
 
-  hide(): void {
-    this.isOpen = false;
-    this.$element.hide();
+  checkLabelOverflow(): boolean {
+    
+    const $labels = this.$gallery.find('.info .label');
+    for (let i = 0; i < $labels.length; i++) {
+      const label = $labels[i];
+      if (label.scrollWidth > label.clientWidth) {
+        return true; 
+      }
+    }
+    return false; 
   }
 
-  resize(): void {
-    super.resize();
-    const $main: JQuery = this.$gallery.find(".main");
-    const $header: JQuery = this.$gallery.find(".header");
-    $main.height(this.$element.height() - $header.height());
+toggleLabelExtension(extendLabels: boolean): void {
+  if (extendLabels) {
+    
+    this.$gallery.find('.info .label').css({
+      'overflow-x': 'visible',
+      'text-overflow': 'nowrap',
+      'white-space': 'break-spaces',
+      'max-width': '100%',
+    });
+  } else {
+    
+    this.$gallery.find('.info .label').css({
+      'overflow-x': 'hidden',
+      'text-overflow': 'ellipsis',
+      'white-space': 'nowrap',
+      'max-width': '100%',
+    });
   }
+}
+
+public setup(): void {
+  const that = this;
+
+  this.galleryComponent = new GalleryComponent({
+    target: <HTMLElement>this.$gallery[0],
+  });
+
+  this.galleryComponent.on(
+    "thumbSelected",
+    function(thumb: any) {
+      that.extensionHost.publish(IIIFEvents.GALLERY_THUMB_SELECTED, thumb);
+      that.extensionHost.publish(IIIFEvents.THUMB_SELECTED, thumb);
+    },
+    false
+  );
+
+  this.galleryComponent.on(
+    "decreaseSize",
+    function() {
+      that.extensionHost.publish(IIIFEvents.GALLERY_DECREASE_SIZE);
+    },
+    false
+  );
+
+  this.galleryComponent.on(
+    "increaseSize",
+    function() {
+      that.extensionHost.publish(IIIFEvents.GALLERY_INCREASE_SIZE);
+    },
+    false
+  );
+}
+
+public databind(): void {
+  this.galleryComponent.options.data = this.galleryData;
+  this.galleryComponent.set(this.galleryData);
+  this.resize();
+}
+
+show(): void {
+  this.isOpen = true;
+  this.$element.show();
+
+  // todo: would be better to have no imperative methods on components and use a reactive pattern
+  setTimeout(() => {
+    this.galleryComponent.selectIndex(this.extension.helper.canvasIndex);
+  }, 10);
+}
+
+hide(): void {
+  this.isOpen = false;
+  this.$element.hide();
+}
+
+resize(): void {
+  super.resize();
+  const $main: JQuery = this.$gallery.find(".main");
+  const $header: JQuery = this.$gallery.find(".header");
+  $main.height(this.$element.height() - $header.height());
+}
 }
