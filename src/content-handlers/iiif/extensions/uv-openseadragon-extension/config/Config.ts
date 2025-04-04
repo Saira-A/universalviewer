11e4c66fe93d97512a3d3e8210b1d71cc375040a
Saira-A <139405429+Saira-A@users.noreply.github.com>
2025-04-04 16:57:02 +0100

    move icons

--- src/content-handlers/iiif/extensions/uv-openseadragon-extension/config/Config.ts
+++ src/content-handlers/iiif/extensions/uv-openseadragon-extension/config/Config.ts
@@ -145,6 +145,8 @@ type OpenSeadragonCenterPanelContent = CenterPanelContent & {
   zoomIn: string;
   zoomOut: string;
   adjustImage: string;
+  oneUp: string;
+  twoUp: string;
 };
 
 type OpenSeadragonCenterPanel = ModuleConfig & {
