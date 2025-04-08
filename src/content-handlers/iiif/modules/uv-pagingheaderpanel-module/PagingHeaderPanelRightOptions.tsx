edfc78b29a01f05dc36e49b6b04d21f86761bc49
jamesmisson <jcmisson@gmail.com>
2025-04-08 16:05:08 +0100

    add icon library, insert icons, add tooltips

--- src/content-handlers/iiif/modules/uv-pagingheaderpanel-module/PagingHeaderPanelRightOptions.tsx
+++ src/content-handlers/iiif/modules/uv-pagingheaderpanel-module/PagingHeaderPanelRightOptions.tsx
@@ -1,20 +1,52 @@
 import React from 'react'
 import HeaderButton from "../uv-shared-module/HeaderButton"
+import { Print, Download, FullScreen, Bookmark, ExitFullScreen, Settings, Share } from "../../../../icons/icons"
 
 const PagingHeaderPanelRightOptions = () => {
   return (
     <>
       <HeaderButton
         onClick={() => console.log('Right options button test')} 
-        label="Test button">
-        T
-        {/* icon goes here */}
+        label="Test button"
+        title="test">
+          <Print />
       </HeaderButton>
       <HeaderButton
         onClick={() => console.log('Left options button test')} 
-        label="Test button">
-        T
-        {/* icon goes here */}
+        label="Test button"
+        title="test">
+          <Download />
+      </HeaderButton>
+
+            <HeaderButton
+        onClick={() => console.log('Left options button test')} 
+        label="Test button"
+        title="test">
+          <Bookmark />
+        </HeaderButton>
+          <HeaderButton
+        onClick={() => console.log('Left options button test')} 
+        label="Test button"
+        title="test">
+          <ExitFullScreen />
+      </HeaderButton>
+      <HeaderButton
+        onClick={() => console.log('Left options button test')} 
+        label="Test button"
+        title="test">
+          <Settings />
+      </HeaderButton>
+      <HeaderButton
+        onClick={() => console.log('Left options button test')} 
+        label="Test button"
+        title="test">
+          <Share />
+      </HeaderButton>
+      <HeaderButton
+        onClick={() => console.log('Left options button test')} 
+        label="Test button"
+        title="test">
+          <FullScreen />
       </HeaderButton>
     </>
   )
